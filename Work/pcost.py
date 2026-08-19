bfa3bf0847df60e708ad06cde98e41d7c7b0b8a8
Joseph Seelin <jdseelin@proton.me>
2026-08-19 21:47:17 +0800

    Update solution to use a function and csv module

--- Work/pcost.py
+++ Work/pcost.py
@@ -2,10 +2,18 @@
 #
 # Exercise 1.27
 
-f = open("Data/portfolio.csv", "rt")
-next(f)
-total = 0
-for line in f:
-    row = line.split(",")
-    total += int(row[1]) * float(row[-1].strip())
-print(total)
+import csv
+
+
+def portfolio_cost(filename):
+    "Computes the total cost of a portfolio file"
+    total = 0
+    with open(filename) as file:
+        rows = csv.reader(file)
+        next(rows)  # Ignore header
+        for row in rows:
+            total += int(row[1]) * float(row[-1])
+    return total
+
+
+print(portfolio_cost("Data/portfolio.csv"))
