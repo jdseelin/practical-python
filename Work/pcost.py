a40bb4c0e8e7ac2425bdad832ab0083db6da282f
Joseph Seelin <jdseelin@proton.me>
2026-08-19 18:07:08 +0800

    Update file with solution

--- Work/pcost.py
+++ Work/pcost.py
@@ -1,3 +1,18 @@
 # pcost.py
 #
 # Exercise 1.27
+# Open file
+# Skip header
+# Loop through the lines
+# Convert line to list
+# Multiply line[1] to line[2]
+# Store running sum to total
+# Print total
+
+f = open("Data/portfolio.csv", "rt")
+next(f)
+total = 0
+for line in f:
+    row = line.split(",")
+    total += int(row[1]) * float(row[-1].strip())
+print(total)
