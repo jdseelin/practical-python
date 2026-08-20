1e543b3dfa6ad5b0ac2113c0823896ae29181e89
Joseph Seelin <jdseelin@proton.me>
2026-08-20 18:40:05 +0800

    Solution to exercise 2.4

--- Work/report.py
+++ Work/report.py
@@ -1,3 +1,19 @@
 # report.py
 #
 # Exercise 2.4
+import csv
+
+
+def read_portfolio(filename):
+    """Computes the total cost (shares * price) of a portfolio file"""
+    portfolio = []
+    with open(filename, "rt") as f:
+        reader = csv.reader(f)
+        next(reader)  # Remove header
+        for name, shares, price in reader:
+            portfolio.append((name, int(shares), float(price)))
+
+    return portfolio
+
+
+print(read_portfolio("Data/portfolio.csv"))
