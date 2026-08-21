bd8c924d55b8507c8fb41a814a63933c4e603dbc
Joseph Seelin <jdseelin@proton.me>
2026-08-21 11:46:29 +0800

    Submit solution to exercise 2.11

--- Work/report.py
+++ Work/report.py
@@ -58,5 +58,8 @@ def make_report(portfolio, prices):
     return report
 
 
+headers = ("Name", "Shares", "Price", "Change")
+print(f"{headers[0]:>10s} {headers[1]:>10s} {headers[2]:>10s} {headers[3]:>10s}")
+print(("-" * 10 + " ") * len(headers))
 for name, shares, price, change in make_report(portfolio, prices):
     print(f"{name:>10s} {shares:>10d} {price:>10.2f} {change:>10.2f}")
